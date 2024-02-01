2b1eb6a310f59898588895cefb8f5c29a119c555
Jayson Cabanglan <jayson.cabanglan@accenture.com>
2024-02-01 04:03:38 +0000

    DAY3 SapUI

--- app/testproject/webapp/controller/Homepage.controller.js
+++ app/testproject/webapp/controller/Homepage.controller.js
@@ -1,21 +1,11 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    // "sap/ui/model/resource/RecourceModel"
+    "sap/ui/core/mvc/Controller"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageToast) {
+    function (Controller) {
         "use strict";
-
         return Controller.extend("testproject.testproject.controller.Homepage", {
-            onPressBtn1(){
-                const oBundle = this.getView().getModel("i18n").getResourceBundle();
-                const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-                const sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-                MessageToast.show(sMsg);
-            }
         });
     });
