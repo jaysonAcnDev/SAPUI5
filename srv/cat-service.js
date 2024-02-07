d16ba2da63d95dd6ffa56cc0d304ad3ad4059d60
Jayson Cabanglan <jayson.cabanglan@accenture.com>
2024-02-07 08:02:20 +0000

    CAP Day2

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -1,9 +1,20 @@
 const cds = require('@sap/cds')
 const axios = require('axios')
+const cloudSDK = require('@sap-cloud-sdk/core')
 
 module.exports = cds.service.impl((srv) => {
     srv.on('test', async (req) => {
-        let my_array = ['1', '2']
-        return 'Hello World';
+        // let oDestination = await cloudSDK.getDestination("swapi")
+        // let oRequestConfig = await cloudSDK.buildHttpRequest(oDestination)
+
+        let callPeopleAPI = await axios.get('https://www.swapi.tech/api/people' , {
+            headers: {}
+        })
+        // let callPeopleAPI = await axios.get(oRequestConfig , {
+        //     headers: {}
+        // })
+        console.log(callPeopleAPI.data)
+        return JSON.stringify(callPeopleAPI.data)
     })
+
 })
